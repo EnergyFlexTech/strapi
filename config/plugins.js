57126828bf93ac1e037d8fdc0bc57c5178a29b14
Momar Hughes <momar.hughes@energyflex.com.au>
2023-11-09 14:59:59 +1000

    chore: intentionally try to break the build to test plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,28 +1,31 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "@strapi/provider-upload-aws-s3",
-      providerOptions: {
-        s3Options: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-          region: env("AWS_REGION", "ap-southeast-2"),
-          params: {
-            ACL: null,
-            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-            Bucket: env("AWS_BUCKET"),
+module.exports = ({ env }) => {
+  console.log("PRODUCTION BUILD");
+  return {
+    upload: {
+      config: {
+        provider: "break-the-build",
+        providerOptions: {
+          s3Options: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+            region: env("AWS_REGION", "ap-southeast-2"),
+            params: {
+              ACL: null,
+              signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+              Bucket: env("AWS_BUCKET"),
+            },
           },
         },
-      },
-      actionOptions: {
-        upload: {
-          ACL: null,
+        actionOptions: {
+          upload: {
+            ACL: null,
+          },
+          uploadStream: {
+            ACL: null,
+          },
+          delete: {},
         },
-        uploadStream: {
-          ACL: null,
-        },
-        delete: {},
       },
     },
-  },
-});
+  };
+};
