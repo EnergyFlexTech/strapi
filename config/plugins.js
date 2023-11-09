1d37bfc3be9afa42545590486e59613cb9cd772e
Momar Hughes <momar.hughes@energyflex.com.au>
2023-11-09 15:13:15 +1000

    chore: try enabled=true in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,31 +1,29 @@
-module.exports = ({ env }) => {
-  console.log("PRODUCTION BUILD");
-  return {
-    upload: {
-      config: {
-        provider: "break-the-build",
-        providerOptions: {
-          s3Options: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-            region: env("AWS_REGION", "ap-southeast-2"),
-            params: {
-              ACL: null,
-              signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-              Bucket: env("AWS_BUCKET"),
-            },
-          },
-        },
-        actionOptions: {
-          upload: {
+module.exports = ({ env }) => ({
+  upload: {
+    enabled: true,
+    config: {
+      provider: "@strapi/provider-upload-aws-s3",
+      providerOptions: {
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+          region: env("AWS_REGION", "ap-southeast-2"),
+          params: {
             ACL: null,
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("AWS_BUCKET"),
           },
-          uploadStream: {
-            ACL: null,
-          },
-          delete: {},
         },
       },
+      actionOptions: {
+        upload: {
+          ACL: null,
+        },
+        uploadStream: {
+          ACL: null,
+        },
+        delete: {},
+      },
     },
-  };
-};
+  },
+});
