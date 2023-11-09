4585211da9e78fd64b9b60369a111d7a0db90c51
Momar Hughes <momar.hughes@energyflex.com.au>
2023-11-09 11:44:40 +1000

    chore: use env vars for media bucket

--- config/plugins.js
+++ config/plugins.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       providerOptions: {
         accessKeyId: env("AWS_ACCESS_KEY_ID"),
         secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: env("AWS_REGION"),
+        region: env("AWS_REGION", "ap-southeast-2"),
         params: {
           ACL: env("AWS_ACL", "private"),
           signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
